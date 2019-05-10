cdb06d1906f7c0ddda1d0a687a86fae51b75a568
leoluyi <leo0650@gmail.com>
2019-05-10 21:38:16 +0800

    Use workdir in docker exec

--- app/run_tests.R
+++ app/run_tests.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(shinytest)
 library(quantmod)
-source("/srv/shiny-server/app/functions.R")
+source("./functions.R")
 options(shiny.testmode=TRUE)
 
 # test xts object
@@ -10,31 +10,33 @@ xt <- xts(x = 1:3, order.by = as.Date(time))
 colnames(xt) <- "test_series"
 
 # test data.frame object
-test_df <- data.frame(date = as.Date(time), 
-                    price = 1:3, 
-                    symbol = rep("test_series",3))
+test_df <- data.frame(
+  date = as.Date(time), 
+  price = 1:3, 
+  symbol = rep("test_series",3)
+)
 
 # api test
 test_that("API works", {
-        
-        expect_error(getSymbols("SPY",
-                                src = "yahoo", 
-                                from = Sys.Date() - 10),
-                     regexp = NA
-)
+  expect_error(
+    getSymbols("SPY",
+               src = "yahoo", 
+               from = Sys.Date() - 10),
+    regexp = NA
+  )
 })
 
 # xts to df test 
 test_that("Function one works", {
-        
-        expect_equal(df_xts(xt), test_df,
-        check.attributes = FALSE)
+  expect_equal(df_xts(xt), test_df,
+               check.attributes = FALSE)
 })
 
 # shiny app test
 test_that("Application works", {
-
-        expect_pass(testApp("/srv/shiny-server/app/",
-                            testnames = "dates",
-                            compareImages = FALSE))
+  expect_pass(
+    testApp(".",
+            testnames = "dates",
+            compareImages = FALSE)
+  )
 })
